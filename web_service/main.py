b4b5c31d57fb5801aee8e488561c12e6935fde53
grk717 <paulgrinkevich@mail.ru>
2022-09-09 20:22:51 +0300

    added forms

--- web_service/main.py
+++ web_service/main.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["POST", "GET"])
 def hello_world():  # put application's code here
+    if request.method == "POST":
+        logo_number = request.form.get("logo_number")
+        print(logo_number)
     return render_template('index.html')
 
 
